32e382a9300eb8f199c7a32a1d97672e3f18191f
Sostenes Dias <sostenes.dev@gmail.com>
2020-11-14 10:20:47 -0300

    Get images from pokemon.com

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -9,8 +9,9 @@ export class ListComponent implements OnInit {
 
   pokemonList = [
     {name: "Bulbasaur",number:1},
-    {name: "Charmander",number:2},
-    {name: "Pikachu",number:3},
+    {name: "Charmander",number:4},
+    {name: "Squirtle",number:7},
+    {name: "Pikachu",number:25},
   ];
   constructor() { }
 
