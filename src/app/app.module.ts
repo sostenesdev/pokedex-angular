f6c4a5b878ab8e23b4c816442812df691585ea78
Sóstenes Dias de Oliveira Junior <sostenes.dev@gmail.com>
2020-11-22 14:40:15 -0300

    Componentizando item da lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
 import { FormsModule } from '@angular/forms';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.css']
+})
+export class ListItemComponent implements OnInit {
+
+  @Input() pokemon:any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
